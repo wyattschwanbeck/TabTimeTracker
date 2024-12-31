94b029237e6a240511e2c3283d2ed847591bc765
Wyatt Schwanbeck <wyattschwanbeck@gmail.com>
2024-12-31 16:20:40 -0500

    Update background.js
    added mutex for saving elapsed time and updating total time on stopwatch.

--- sidebar/background.js
+++ sidebar/background.js
@@ -1,4 +1,3 @@
-
 var priorUrl;
 var currentTab;
 var startTime;
@@ -10,66 +9,98 @@ var total = 0;
 var stopwatchInterval; // to keep track of the interval 
 var elapsedPausedTime;
 
+class Mutex {
+  constructor() {
+    this.locked = false;
+    this.queue = [];
+  }
 
+  lock() {
+    return new Promise((resolve) => {
+      if (!this.locked) {
+        this.locked = true;
+        resolve();
+      } else {
+        this.queue.push(resolve);
+      }
+    });
+  }
+
+  unlock() {
+    if (this.queue.length > 0) {
+      const resolve = this.queue.shift();
+      resolve();
+    } else {
+      this.locked = false;
+    }
+  }
+}
+
+const mutex = new Mutex();
 startTimer();
 
-function saveElapsed() {
-    let new_url;
-    browser.tabs.query({
-        active: true,
-        currentWindow: true
-    }).then(tabs => {
-        new_url = tabs[0].url;
+async function saveElapsed() {
+    await mutex.lock();
+    try {
+        let new_url;
+        browser.tabs.query({
+            active: true,
+            currentWindow: true
+        }).then(tabs => {
+            new_url = tabs[0].url;
 
-        if (priorUrl !== new_url) {
-            contentToStore[`ElapsedTime${currentTab}`] = elapsedTime;
-            browser.storage.local.set(contentToStore);
-            let priorElapsedTime = elapsedTime;
-            stopStopwatch();
-            resetStopwatch();
-            
-            
-            elapsedTime= 0;
-            browser.storage.local.get(`ElapsedTime${new_url}`).then((storedInfo) => {
-                
-                elapsedPreviousTime = storedInfo[Object.keys(storedInfo)[0]];
-                if(typeof elapsedPreviousTime === "undefined"){
-                    elapsedPreviousTime=0;
-                }
-               
+            if (priorUrl !== new_url) {
+                contentToStore[`ElapsedTime${currentTab}`] = elapsedTime;
+                browser.storage.local.set(contentToStore);
+                let priorElapsedTime = elapsedTime;
+                stopStopwatch();
+                resetStopwatch();
                 
                 
-                
-                let lastPathArray = priorUrl.split('/');
-                let lastHost = lastPathArray[2];
-               
-                priorUrl = currentTab;
-                
-                currentTab = new_url;
-                //Retrieve elapsed time spent on website based on existing keys
-                let pathArray = currentTab.split('/');
-                
-                let protocol = pathArray[0];
-                let host = pathArray[2];
-                let url = protocol + '//' + host;
-                
-                
-                total = 0;
-                
+                elapsedTime= 0;
+                browser.storage.local.get(`ElapsedTime${new_url}`).then((storedInfo) => {
+                    
+                    elapsedPreviousTime = storedInfo[Object.keys(storedInfo)[0]];
+                    if(typeof elapsedPreviousTime === "undefined"){
+                        elapsedPreviousTime=0;
+                    }
+                   
+                    
+                    
+                    
+                    let lastPathArray = priorUrl.split('/');
+                    let lastHost = lastPathArray[2];
+                   
+                    priorUrl = currentTab;
+                    
+                    currentTab = new_url;
+                    //Retrieve elapsed time spent on website based on existing keys
+                    let pathArray = currentTab.split('/');
+                    
+                    let protocol = pathArray[0];
+                    let host = pathArray[2];
+                    let url = protocol + '//' + host;
+                    
+                    
+                    total = 0;
+                    
 
-                browser.storage.local.get().then((storedInfo) => {
-                    Object.keys(storedInfo).forEach((key) => {
-                        if (key.startsWith(`ElapsedTime${url}`) && key !== `ElapsedTime${currentTab}`) {
-                            total += storedInfo[key];
-                        }
+                    browser.storage.local.get().then((storedInfo) => {
+                        Object.keys(storedInfo).forEach((key) => {
+                            if (key.startsWith(`ElapsedTime${url}`) && key !== `ElapsedTime${currentTab}`) {
+                                total += storedInfo[key];
+                            }
+                        });
+                    startTime = new Date().getTime() - elapsedPreviousTime;
+                    startTimer();
+
                     });
-                startTime = new Date().getTime() - elapsedPreviousTime;
-                startTimer();
-
                 });
-            });
-        }
-    });
+            }
+        });
+    } finally {
+        mutex.unlock();
+    }
 }
 
 function startTimer() {
@@ -116,7 +147,7 @@ function formatElapsed(elapsedTime) {
 }
 
 function resetStopwatch() {
-
+    
     stopStopwatch(); // stop the interval
     elapsedPausedTime = 0; // reset the elapsed paused time variable
 
@@ -124,8 +155,11 @@ function resetStopwatch() {
 
     //document.getElementById("stopwatch").innerText = formatElapsed(elapsedTime); // reset the display
 }
+const update_stopwatch_mutex = new Mutex()
 
-function updateStopwatch() {
+async function updateStopwatch() {
+    await update_stopwatch_mutex.lock();
+    try {
     //milliseconds
     currentTime = new Date().getTime(); // get current time in milliseconds
     if(typeof elapsedPreviousTime === "undefined") {
@@ -133,8 +167,14 @@ function updateStopwatch() {
     }
     elapsedTime = currentTime - (startTime);
     window.elapsedTime = elapsedTime;
-
+    
     browser.runtime.sendMessage({"elapsedTime": elapsedTime, "total":total+elapsedTime});
+    }catch (error) {
+        // On a browser window without onActiveChanged i.e not a 'tab'
+    }
+     finally {
+        update_stopwatch_mutex.unlock();
+    }
 
 }
 
@@ -149,7 +189,8 @@ browser.tabs.onUpdated.addListener(saveElapsed);
 try {
     browser.tabs.onActiveChanged.addListener(saveElapsed);
     } catch (error) {
-        console.error(error);
+        // On a browser window without onActiveChanged i.e not a 'tab'
+        
     }
 
 browser.tabs.onRemoved.addListener(saveElapsed);
